1340be208bb7368b4034152f92374ef3cdf8fe01
GurumanikulaAjay <97425496+GurumanikulaAjay@users.noreply.github.com>
2022-06-29 11:31:25 +0530

    n=int(input()) arr=list(map(int,input().split())) s=1 for i in range(n):     s=1     for j in range(n):         if(i!=j):             s*=arr[j]     print(s,end=" ")

---
+++ _Product_of_Array_Except_Self.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+n=int(input())
+arr=list(map(int,input().split()))
+s=1
+for i in range(n):
+    s=1
+    for j in range(n):
+        if(i!=j):
+            s*=arr[j]
+    print(s,end=" ")
