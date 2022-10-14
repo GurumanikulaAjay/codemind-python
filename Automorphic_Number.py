330649cafc89ad8e0adedbd619c30e9c52864bc4
GurumanikulaAjay <97425496+GurumanikulaAjay@users.noreply.github.com>
2022-10-14 18:48:24 +0530

    n=int(input()) v=str(n) r=n**2 r=str(r) if r.endswith(v):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,14 +1,8 @@
-import math
 n=int(input())
-g=n
-r=n*n
-c=0
-while n>0 :
-    v=n%10
-    c=c+1
-    n=n//10
-x=r% math.pow(10,c)
-if(x==g) :
-    print('Automorphic Number')
-else :
-    print('Not an Automorphic Number')
+v=str(n)
+r=n**2
+r=str(r)
+if r.endswith(v):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
