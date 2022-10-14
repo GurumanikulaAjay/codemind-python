c3f428b93175437499356abaff0a1084d70d4c9d
GurumanikulaAjay <97425496+GurumanikulaAjay@users.noreply.github.com>
2022-10-14 18:18:09 +0530

    def prime(a):     c=0     for i in range(1,a):         if(a%i==0):             c+=1     if(c==1):         return 1     else:         return 0 n=int(input()) v=r=d=x=f=0 x=n while x>0:     f+=1     x=x//10 if(prime(n)==1):     v=n     while(v>0):         r=v%10         if(prime(r)==1):             d+=1         else:             break         v=v//10     if(f==d):         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,19 +1,30 @@
-n=int(input())
-a=n
-for i in range(2,int(n**0.5)+1):
-    if n%i==0:
-        print('Not Mega Prime')
-        break
-else:
-    d=0
-    pd=0
-    while n:
-        rem=n%10
-        n=n//10
-        d+=1
-        if rem==2 or rem==3 or rem==5 or rem==7:
-            pd+=1
-    if d==pd:
-        print('Mega Prime')
+def prime(a):
+    c=0
+    for i in range(1,a):
+        if(a%i==0):
+            c+=1
+    if(c==1):
+        return 1
     else:
-        print('Not Mega Prime')
+        return 0
+n=int(input())
+v=r=d=x=f=0
+x=n
+while x>0:
+    f+=1
+    x=x//10
+if(prime(n)==1):
+    v=n
+    while(v>0):
+        r=v%10
+        if(prime(r)==1):
+            d+=1
+        else:
+            break
+        v=v//10
+    if(f==d):
+        print("Mega Prime")
+    else:
+        print("Not Mega Prime")
+else:
+    print("Not Mega Prime")
